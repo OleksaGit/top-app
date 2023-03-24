13361a4150cc2c6350d9e332e8e59b4be4cd50a1
Alex <alexandr.pogre@gmail.com>
2023-03-24 18:06:04 +0200

    add some components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {Button, Htag} from "@/components";
+import {Button, Htag, P, Tag} from "@/components";
 
 
 export default function Home(): JSX.Element {
@@ -7,6 +7,12 @@ export default function Home(): JSX.Element {
             <Htag tag='h1'>Text</Htag>
             <Button appearance='primary' arrow='down'>Кнопка</Button>
             <Button appearance='ghost' arrow='down'>Кнопка</Button>
+            <P size='s'>paragraph text</P>
+            <P size='m'>paragraph text</P>
+            <P size='l'>paragraph</P>
+            <Tag size='s'> s tag</Tag>
+            <Tag size='m' color='red'> s tag asfd</Tag>
+            <Tag size='m' color='primary' href='https://google.com'> посилання на домашню гугла :)</Tag>
         </>
     );
 }
