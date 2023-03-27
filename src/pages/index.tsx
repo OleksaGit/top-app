74c86413906f2f85ae165852b030ed4f3136f02f
Alex <alexandr.pogre@gmail.com>
2023-03-27 17:21:44 +0300

    add rating component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
-import {Button, Htag, P, Tag} from "@/components";
+import {Button, Htag, P, Rating, Tag} from "@/components";
+import {useEffect, useState} from "react";
 
 
 export default function Home(): JSX.Element {
+    //const [counter, setCounter] = useState<number>(0);
+    const [rating, setRating] = useState<number>(4);
+
     return (
         <>
-            <Htag tag='h1'>Text</Htag>
-            <Button appearance='primary' arrow='down'>Кнопка</Button>
+            <Htag tag='h1'>counter</Htag>
+            <Button appearance='primary' arrow='right' onClick={() => setCounter(x => x+1)}>Кнопка</Button>
             <Button appearance='ghost' arrow='down'>Кнопка</Button>
             <P size='s'>paragraph text</P>
             <P size='m'>paragraph text</P>
@@ -13,6 +17,8 @@ export default function Home(): JSX.Element {
             <Tag size='s'> s tag</Tag>
             <Tag size='m' color='red'> s tag asfd</Tag>
             <Tag size='m' color='primary' href='https://google.com'> посилання на домашню гугла :)</Tag>
+            {/*<Rating rating={4} isEditable/>*/}
+            <Rating rating={rating} isEditable setRating={setRating}/>
         </>
     );
 }
